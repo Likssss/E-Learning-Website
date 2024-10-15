9cb5f1f3b454d3fde85d07f956955e4a427f50ba
Likssss <148707565+Likssss@users.noreply.github.com>
2024-10-15 18:36:34 +0800

    Uploaded the website from local machine

---
+++ components/connect.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "rootuser123";
+$dbname = "dbtests1";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+   function unique_id() {
+      $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+      $rand = array();
+      $length = strlen($str) - 1;
+      for ($i = 0; $i < 20; $i++) {
+          $n = mt_rand(0, $length);
+          $rand[] = $str[$n];
+      }
+      return implode($rand);
+   }
+
+?>
---
+++ components/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+
+   &copy; copyright @ <?= date('Y'); ?> by <span>CompanyName</span> | all rights reserved!
+
+</footer>
